343037cf964291877ddd68963599412c1c81a304
Dmitriy <dmitriypruss21041984@gmail.com>
2021-05-27 23:49:45 +0300

    2 commit

--- js/index.js
+++ js/index.js
@@ -6,39 +6,60 @@
 // разных типов с оповещением пользователя о типе наступившей ошибки.
 
 const pow = (num, n) => {
-    if (n === 0) {
-        return 1;
-    };
-    if (n > 1000) {
-        throw new ReferenceError(`Pow ${n} is so big!`);
-    };
-    if (n >= 1) {
-        return num * pow(num, n-1);
-    };
-    if (n < 0) {
-        return 1 / num * pow(num, n + 1);
-    }
-
     if (typeof num !== 'number') {
         throw new TypeError(`Value ${num} is not a number`);
     };
     if (typeof n !== 'number') {
         throw new TypeError(`Pow ${n} is not a number`);
     };
+    if (n > 2000) {
+        throw new ReferenceError(`Pow ${n} is so big!`);
+    };
+    if (n % 1 !== 0) {
+        throw new ReferenceError(`Pow ${n} must be an integer number!`);
+    };
+
+    switch (true) {
+        case (n === 0):
+            return 1;
+            break;
+        case (n === 1):
+            return num;
+            break;
+        case (n >= 1):
+            return num * pow(num, n-1);
+            break;
+        case (n < 0):
+            return 1 / num * pow(num, n + 1);
+            break;
+    };
 };
 
-console.log('pow(5, 3) :>> ', pow(5, 3));
-console.log('pow(5, 0) :>> ', pow(5, 0));
-
-
 try {
+    console.log('pow(5, 3) :>> ', pow(5, 3));
+    console.log('pow(5, 0) :>> ', pow(5, 0));
+
+    // TypeError
     // console.log('pow(5, "asd") :>> ', pow(5, 'asd'));
-    console.log('pow(5, "asd") :>> ', pow(5, 2000));
+    // console.log('pow(5, "asd") :>> ', pow('asda', 3));
+
+    // RangeError
+    console.log('pow(5, "asd") :>> ', pow(5, 2.578));
+    // console.log('pow(5, "asd") :>> ', pow(5, 2578));  
+
 } catch (error) {
+    console.log('error.message :>> ', error.message);
+
     if (error instanceof TypeError) {
         console.log(error.name + ': you must input a number');
     };
     if (error instanceof ReferenceError) {
         console.log(error.name + ': you must correct your code');
     };
-}
+};
+
+
+
+
+  
+
